2fd0d251b71eed4b4351c45cd2476a10173fed23
Olivier Diotte <olivier@diotte.ca>
2022-01-26 21:17:35 -0500

    Rename route

--- mind_map/routes.py
+++ mind_map/routes.py
@@ -7,7 +7,7 @@ from nodes import *
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/<path:endpoint_path>', methods=['GET', 'POST'])
-def home(endpoint_path=None):
+def mind_map(endpoint_path=None):
     response_kwargs = {'mimetype': 'application/json', 'status': 500}
     output_full_tree = 'tree' in request.args
 
